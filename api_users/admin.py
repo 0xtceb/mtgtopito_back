23b9bdba59701d43ce70e2591bf81087d7bf354b
luc <akbal.luc@gmail.com>
2019-05-16 17:33:47 +0000

    Added Ligues And Scores logic to the models

--- api_users/admin.py
+++ api_users/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from api_users.models import Deck, Card, Ligue, Score
 
 # Register your models here.
+
+admin.site.register(Deck)
+admin.site.register(Card)
+admin.site.register(Ligue)
+admin.site.register(Score)
--- api_users/models.py
+++ api_users/models.py
@@ -8,9 +8,34 @@ class Deck(models.Model):
     user = models.ForeignKey(User, default=None, blank=True)
     commander = models.ForeignKey("Card", default=None, blank=True)
     name = models.CharField(max_length=200)
+    ligues = models.ManyToManyField("Ligue", default=None, blank=True)
+
+    def __str__(self):
+        return '%s' % self.name
 
 class Card(models.Model):
     multiverseid = models.IntegerField(default=1)
     name = models.CharField(max_length=200)
     imageUrl = models.CharField(max_length=1000)
+    decks = models.ManyToManyField(Deck, default=None, blank=True)
+
+    def __str__(self):
+        return '%s' % self.name
+
+class Ligue(models.Model):
+    name = models.CharField(max_length=200)
     decks = models.ManyToManyField(Deck)
+
+    def __str__(self):
+        return '%s : %s joueur(s)' % (self.name, self.decks.count())
+
+class Score(models.Model):
+    ligue =  models.ForeignKey(Ligue)
+    deck_one = models.ForeignKey(Deck, related_name='deck_one', default=None)
+    deck_two = models.ForeignKey(Deck, related_name='deck_two', default=None)
+    results = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '%s VS %s | score %s' % (self.deck_one.name,
+                                        self.deck_two.name,
+                                        self.results)
