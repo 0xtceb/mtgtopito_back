69704aa5e2ac56b146fd9e5cf3e5c834db586ffa
luc <akbal.luc@gmail.com>
2019-05-20 17:32:28 +0000

    Modified admin appearance for deck edition

--- api_users/admin.py
+++ api_users/admin.py
@@ -6,7 +6,20 @@ from api_users.models import Deck, Card, DeckCard, Ligue, Score
 
 # Register your models here.
 
-admin.site.register(Deck)
+class ChoiceInline(admin.TabularInline):
+    model = DeckCard
+
+class DeckAdmin(admin.ModelAdmin):
+
+    #def card_number(self, instance):
+    #    return DeckCard.objects.filter(deck=instance).count()
+
+    list_display = ['commander', 'name', 'card_number']
+    list_filter = ('commander','name')
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Deck, DeckAdmin)
 admin.site.register(Card)
 admin.site.register(DeckCard)
 admin.site.register(Ligue)
