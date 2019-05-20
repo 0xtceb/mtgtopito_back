f87288af4ce9e8b24bf9e7062a09198ad5403737
luc <akbal.luc@gmail.com>
2019-05-20 17:33:55 +0000

    Modified relationship between deck and card

--- api_users/models.py
+++ api_users/models.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
 class Deck(models.Model):
     user = models.ForeignKey(User, default=None, blank=True)
-    commander = models.ForeignKey("Card", default=None, blank=True)
+    commander = models.ForeignKey("Card", default=None, blank=True, related_name='commander')
     name = models.CharField(max_length=200)
     ligues = models.ManyToManyField("Ligue", default=None, blank=True)
 
@@ -17,7 +17,7 @@ class Card(models.Model):
     multiverseid = models.IntegerField(default=1)
     name = models.CharField(max_length=200)
     imageUrl = models.CharField(max_length=1000)
-    decks = models.ManyToManyField(Deck, default=None, blank=True)
+
 
     def __str__(self):
         return '%s' % self.name
